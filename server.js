2f6329ec134fb4d82d5b291b889fe408e473212f
João Pedro <48479300+ds-jp@users.noreply.github.com>
2023-05-27 22:28:10 -0300

    Reorganização da estrutura de pastas

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const mustacheExpress = require("mustache-express");
-const indexRouter = require("./routes/index");
+const indexRouter = require("./src/routes/index");
 
 const app = express();
 
 app.engine("mustache", mustacheExpress());
 app.set("view engine", "mustache");
-app.set("views", __dirname + "/views");
-app.use(express.static("public"));
+app.set("views", __dirname + "/src/views");
+app.use(express.static("./src/public"));
 app.use("/", indexRouter);
 
 const PORT = 3000;

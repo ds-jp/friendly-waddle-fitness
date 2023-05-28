1e95585c020fdd6ff34aabddc097576ced85b5c0
João Pedro <48479300+ds-jp@users.noreply.github.com>
2023-05-28 16:05:27 -0300

    Implementação do layout, criação da página "Saiba Mais" e atualização do header e index

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,17 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   const data = {
+    title: "FriendlyWaddle Fitness",
     name: "Teste",
   };
   res.render("index", data);
+});
+
+router.get("/saiba-mais", (req, res) => {
+  const data = {
+    title: "Saiba Mais - FriendlyWaddle Fitness",
+  };
+  res.render("saiba-mais", data);
 });
 
 module.exports = router;

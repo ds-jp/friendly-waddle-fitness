8c8136687da8a1d33b313a56f79ca2fb228aa10e
João Pedro <48479300+ds-jp@users.noreply.github.com>
2023-05-30 11:35:54 -0300

    Mover server.js, atualizar scripts no package.json e ajuste de estilo do index.js.

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const mustacheExpress = require("mustache-express");
-const indexRouter = require("./src/routes/index");
+const indexRouter = require("./routes/index");
 
 const app = express();
 
 app.engine("mustache", mustacheExpress());
 app.set("view engine", "mustache");
-app.set("views", __dirname + "/src/views");
+app.set("views", __dirname + "/views");
 app.use(express.static("./src/public"));
 app.use("/", indexRouter);
 
